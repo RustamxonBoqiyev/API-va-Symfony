d6fc594947d32d73a9424ddbc4f61e0724f00db0
Rustamxon Boqiyaev <brustamxon@gmail.com>
2022-08-04 19:16:33 +0500

    User va get post va boshqa end poit lar qo'shiladi

---
+++ src/Entity/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\UserCreateAction;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    collectionOperations: [
+        'get',
+        'createUser'=>[
+            'method'=>'post',
+            'path'=>'/user/my',
+            'controllar'=>UserCreateAction::class
+        ]
+    ],
+    itemOperations: ['delete','get'],
+    denormalizationContext: ['groups'=>['user:write']],
+    normalizationContext: ['groups'=>['user:read']]
+)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    #[Groups(['user:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:write'])]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
